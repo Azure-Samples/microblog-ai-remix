47b5919397721e4be576d046593648e15a4c5e2b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-03-05 01:11:12 -0300

    fix: update SSR handler to use Remix Express and improve compatibility response

--- server/src/functions/ssr.ts
+++ server/src/functions/ssr.ts
@@ -1,8 +1,9 @@
 import { app } from '@azure/functions';
-import { createRequestHandler } from '@scandinavianairlines/remix-azure-functions';
+
+import { createRequestHandler } from '@remix-run/express';
 
 import path from 'path';
-import { pathToFileURL , fileURLToPath } from 'url';
+import { pathToFileURL, fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const buildPath = path.resolve(__dirname, '../../../../build/server/index.js');
@@ -13,20 +14,28 @@ const build = await import(buildUrl);
 app.setup({ enableHttpStream: true });
 
 app.http('ssr', {
-    methods: [
-        'GET',
-        'POST',
-        'DELETE',
-        'HEAD',
-        'PATCH',
-        'PUT',
-        'OPTIONS',
-        'TRACE',
-        'CONNECT'
-    ],
-    authLevel: 'anonymous',
-    handler: createRequestHandler({
-        build,
-    }),
-    route: 'ssr/{*path}',
+  methods: [
+    'GET',
+    'POST',
+    'DELETE',
+    'HEAD',
+    'PATCH',
+    'PUT',
+    'OPTIONS',
+    'TRACE',
+    'CONNECT'
+  ],
+  authLevel: 'anonymous',
+  handler: (request, context) => {
+    const expressHandler = createRequestHandler({
+      build,
+      mode: process.env.NODE_ENV,
+    });
+
+    return {
+      status: 200,
+      jsonBody: 'Function is configured for compatibility only'
+    }
+  },
+  route: '{*path}',
 });
